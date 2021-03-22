83ac34dbb175263f9ecad57900d021314af8b5ab
HannahDi <github@hannah.dienhart.de>
2021-03-22 21:55:04 +0100

    feat(excel2json): add __getReactions() for list of reaction dicts in json, still missing repicates data

--- notebooks/converter/converter.py
+++ notebooks/converter/converter.py
@@ -17,6 +17,7 @@ class ConverterExcelJSON():
         TODO DOKU
         '''
         self.path = path
+        self.indexFinder = {}
         self.outJSON: dict[str, object] = {}
         self.__parseXLSX()
         # DEBUG
@@ -63,23 +64,17 @@ class ConverterExcelJSON():
                     values[cond].append(value)
                     iy += y
                     ix += x
-            # we take for all other conditions as many cellvalues as for the first, if they contain nan we save an empty string
+            # we take for all other conditions as many cellvalues as for the first
             else:
                 values[cond] = []
                 if y == 0:
                     for i in range(coords[1] + x, coords[1] + ix):
                         value = df.iloc[coords[0] + iy, i]
-                        if value is np.nan:
-                            values[cond].append('')
-                        else:
-                            values[cond].append(value)
+                        values[cond].append(value)
                 elif x == 0:
                     for i in range(coords[0] + y, coords[0] + iy):
                         value = df.iloc[i, coords[1] + ix]
-                        if value is np.nan:
-                            values[cond].append('')
-                        else:
-                            values[cond].append(value)
+                        values[cond].append(value)
         return values
 
     def __getCoords(self, df, cond):
@@ -96,7 +91,7 @@ class ConverterExcelJSON():
         values = self.__filter(gi, ['Title', 'Date of creation', 'DOI (optional)', 'PubMedID (optional)', 'URL (optional)'], x=1)
         self.outJSON['name'] = values['Title']
         ######## not implemented yet?
-        #self.outJSON['date'] = values['Date of creation'].strftime('%d.%m.%Y')
+        # self.outJSON['date'] = values['Date of creation'].strftime('%d.%m.%Y')
         ######## key 'doi' guessed
         self.outJSON['doi'] = values['DOI (optional)']
         self.outJSON['pubmedID'] = values['PubMedID (optional)']
@@ -112,6 +107,7 @@ class ConverterExcelJSON():
         '''
         self.outJSON['protein'] = []
         values = self.__filter(proteins, ['Name', 'ID', 'Sequence', 'Vessel', 'Constant', 'Source organism', 'EC Number', 'UniProt ID'], y=1, isSingleValue=False)
+        self.indexFinder['proteins'] = values['Name']
         for i in range(len(values['Name'])):
             proteinDic = {}
             # id not wanted
@@ -132,6 +128,9 @@ class ConverterExcelJSON():
                 proteinDic['uniprotid'] = values['UniProt ID'][i]
             # Protein Dic needs substanceunits, these are not given in the excel sheet so I cheat here!!!
             proteinDic['substanceunits'] = 'umole / l'
+            # Protein Dic needs init_conc, which is only given in the data sheet and differs for differnet experiments!!!
+            # So I cheat here again!!!
+            proteinDic['init_conc'] = 1.0
             self.outJSON['protein'].append(proteinDic)
 
     def __getReactants(self, reactants):
@@ -140,6 +139,7 @@ class ConverterExcelJSON():
         '''
         self.outJSON['reactant'] = []
         values = self.__filter(reactants, ['Name', 'ID', 'Vessel', 'Constant', 'SMILES', 'InCHI'], y=1, isSingleValue=False)
+        self.indexFinder['reactants'] = values['Name']
         for i in range(len(values['Name'])):
             reactantDic = {}
             # id not wanted
@@ -155,6 +155,80 @@ class ConverterExcelJSON():
             if not(values['SMILES'][i] is np.nan):
                 reactantDic['smiles'] = values['SMILES'][i]
             self.outJSON['reactant'].append(reactantDic)
+
+    def __getReactions(self, reactions):
+        '''
+        TODO DOKU
+        '''
+        self.outJSON['reaction'] = []
+        values = self.__filter(reactions, ['Name', 'ID', 'Temperature value', 'Temperature unit', 'pH value', 'Educts', 'Products', 'Proteins', 'Modifiers'], y=1, isSingleValue=False)
+        for i in range(len(values['Name'])):
+            reacDic = {}
+            # id not wanted
+            # reacDic['id_'] = values['ID'][i]
+            reacDic['name'] = values['Name'][i]
+            reacDic['temperature'] = values['Temperature value'][i]
+            reacDic['tempunit'] = values['Temperature unit'][i]
+            reacDic['ph'] = values['pH value'][i]
+            # Educts, Products and Proteins can be more than one -> need lists
+            # All need stoich key, but this is not given in excel so I cheat here!!!
+            reacDic['educts'] = []
+            educts = values['Educts'][i].split(', ')
+            for educt in educts:
+                eductDic = {}
+                eductDic['species'] = educt
+                eductDic['stoich'] = 1.0
+                ind = self.indexFinder['reactants'].index(educt)
+                eductDic['constant'] = self.outJSON['reactant'][ind]['constant']
+                eductDic['replicates'] = []
+                reacDic['educts'].append(eductDic)
+            reacDic['products'] = []
+            products = values['Products'][i].split(', ')
+            for product in products:
+                prodDic = {}
+                prodDic['species'] = product
+                prodDic['stoich'] = 1.0
+                ind = self.indexFinder['reactants'].index(product)
+                prodDic['constant'] = self.outJSON['reactant'][ind]['constant']
+                prodDic['replicates'] = []
+                reacDic['products'].append(prodDic)
+            reacDic['proteins'] = []
+            prots = values['Proteins'][i].split(', ')
+            for prot in prots:
+                protDic = {}
+                protDic['species'] = prot
+                protDic['stoich'] = 1.0
+                ind = self.indexFinder['proteins'].index(prot)
+                protDic['constant'] = self.outJSON['protein'][ind]['constant']
+                protDic['replicates'] = []
+                reacDic['proteins'].append(protDic)
+            # Modifiers not always given
+            if not(values['Modifiers'][i] is np.nan):
+                # TODO wants a list
+                reacDic['modifiers'] = []
+                modifiers = values['Proteins'][i].split(', ')
+                for modifier in modifiers:
+                    modDic = {}
+                    modDic['species'] = modifier
+                    modDic['stoich'] = 1.0
+                    try:
+                        ind = self.indexFinder['reactants'].index(modifier)
+                        modDic['constant'] = self.outJSON['reactant'][ind]['constant']
+                    # TODO except...
+                    except ValueError:
+                        pass
+                    try:
+                        ind = self.indexFinder['proteins'].index(modifier)
+                        modDic['constant'] = self.outJSON['protein'][ind]['constant']
+                    except ValueError:
+                        # Should not happen...
+                        modDic['constant'] = True
+                    reacDic['modifiers'].append(modDic)
+            else:
+                reacDic['modifiers'] = []
+            # reaction needs reversible key, this is not given in the excel sheet so I cheat here!!!
+            reacDic['reversible'] = True
+            self.outJSON['reaction'].append(reacDic)
 
     def __getVessel(self, vessels):
         '''
@@ -172,7 +246,7 @@ class ConverterExcelJSON():
     def __getVessels(self, vessels):
         '''
         TODO DOKU
-        This function creates a list of all vessels in the excel-sheet, but the API expects a mapping not a list, 
+        This function creates a list of all vessels in the excel-sheet, but the API expects a mapping not a list,
         maybe this function will be useful later on
         '''
         self.outJSON['vessel'] = []
@@ -189,11 +263,12 @@ class ConverterExcelJSON():
         '''
         TODO DOKU
         '''
-        doc = pd.read_excel(self.path, sheet_name=['General Information', 'Vessels', 'Reactants', 'Proteins'])
+        doc = pd.read_excel(self.path, sheet_name=['General Information', 'Vessels', 'Reactants', 'Proteins', 'Reactions'])
         self.__getGI(doc['General Information'])
         self.__getVessel(doc['Vessels'])
         self.__getReactants(doc['Reactants'])
         self.__getProteins(doc['Proteins'])
+        self.__getReactions(doc['Reactions'])
 
     def __writeJSON(self, path):
         with open(path, 'w', encoding='utf-8') as f:
